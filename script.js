a9e8b119070631a48a132db79e6576c454b83391
Mark <marklajom.work@gmail.com>
2023-01-17 17:00:02 +0100

    Add javascript function to add lists

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-const hamburger = document.querySelector(".hamburger")
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active")
+const LOCAL_STORAGE_LIST_KEY = 'task-lists'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+
+newListForm.addEventListener('submit', e =>{
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  render()
 })
+
+function createList(name){
+  return {id:Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender (){
+  save()
+  render()
+}
+
+function save(){
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+}
+
+function render() {
+  clearElement(listsContainer)
+  lists.forEach(list =>{
+    const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
+    listElement.classList.add("list-name")
+    listElement.innerText = list.name
+    listsContainer.appendChild(listElement)
+  })
+}
+
+function clearElement(element){
+  while (element.firstChild){
+    element.removeChild(element.firstChild)
+  }
+}
+
+render()  
