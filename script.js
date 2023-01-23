3d894f312b716a281eb47441576fc05f9540651c
Mark <marklajom.work@gmail.com>
2023-01-23 17:47:59 +0100

    Add changing task title

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector('[data-tasks]')
 
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId"
 const LOCAL_STORAGE_LIST_KEY = "task.list"
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e=>{
+  if (e.target.tagName.toLowerCase() === 'li'){
+    selectedListId = e.target.dataset.listId
+    saveAndRender()
+  }
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
+})
 
 newListForm.addEventListener('submit', e =>{
   e.preventDefault()
@@ -26,15 +46,30 @@ function saveAndRender (){
 
 function save(){
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 function render() {
   clearElement(listsContainer)
+  renderLists()
+  const selectedList = lists.find(list => list.id === selectedListId)
+  if (selectedListId == null){
+    listDisplayContainer.style.display == 'none'
+  } else{
+    listDisplayContainer.style.display= ''
+    listTitleElement.innerText = selectedList.name
+  }
+}
+
+function renderLists() {
   lists.forEach(list =>{
     const listElement = document.createElement('li')
     listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
     listElement.innerText = list.name
+    if (list.id === selectedListId) {
+      listElement.classList.add("active-list")
+    }
     listsContainer.appendChild(listElement)
   })
 }
