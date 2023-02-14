f69b7243942c7ef2102a36584b8e93d2a5795252
Mark <marklajom.work@gmail.com>
2023-02-14 10:03:36 +0100

    Add sliding menù animation and funcionalities

--- menu-animation.js
+++ menu-animation.js
@@ -1,5 +1,12 @@
 const hamburger = document.querySelector(".hamburger")
+const mobileMenu = document.querySelector(".sliding")
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
+  mobileMenu.classList.toggle("visible")
 })
+
+
+
+
+
