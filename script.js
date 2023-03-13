3661b641d372fecc64c73fdc3f2e2d5e137c0d89
Mark <marklajom.work@gmail.com>
2023-03-13 15:37:58 +0100

    Add changes

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const LOCAL_STORAGE_LIST_KEY = "task.list"
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
+
 listsContainer.addEventListener('click', e=>{
   if (e.target.tagName.toLowerCase() === 'li'){
     selectedListId = e.target.dataset.listId
@@ -75,15 +76,6 @@ function createList(name){
   return {id:Date.now().toString(), name: name, tasks: [] }
 }
 
-function saveAndRender (){
-  save()
-  render()
-}
-
-function save(){
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
-  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
-}
 
 function render() {
   clearElement(listsContainer)
@@ -138,6 +130,16 @@ function clearElement(element){
   while (element.firstChild){
     element.removeChild(element.firstChild)
   }
+}
+
+function saveAndRender (){
+  save()
+  render()
+}
+
+function save(){
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 render()  
